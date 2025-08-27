268f5c5fa09b76ad0b9da017a58d02a7bcf6d46d
노정민 <48047620+yesjm-dev@users.noreply.github.com>
2025-08-27 22:40:49 +0900

    chore: application 모듈 spring boot 추가

--- ratelimiter-application/build.gradle.kts
+++ ratelimiter-application/build.gradle.kts
@@ -1,9 +1,14 @@
 plugins {
     kotlin("jvm")
+    kotlin("plugin.spring") version "1.9.23"
+    id("org.springframework.boot")
+    id("io.spring.dependency-management")
 }
 
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
     implementation(project(":ratelimiter-domain"))
+
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.0")
     testImplementation(kotlin("test"))
